1251774d086f67e815f0ee0dd0ae90063cf4716c
H4M5TER <zhuxx2003@gmail.com>
2020-06-06 01:03:37 +0800

    Update package.bat

--- pkg/package.bat
+++ pkg/package.bat
@@ -1,4 +1,4 @@
 chcp 65001
 cd /D %~dp0
-7za a .\toolkit_%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%-%time:~3,2% ..\bat\视频* .\vendor .\LICENSE.txt .\README.txt
+D:\CLI\7-zip\7za a ".\video_kit_%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%-%time:~3,2%" ..\bat\* ..\bin\* .\vendor .\LICENSE.txt .\README.txt
 pause
