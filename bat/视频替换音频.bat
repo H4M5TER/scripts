fe6d9affd70048cc753ca6ce596649df0499f9e5
H4M5TER <zhuxx2003@gmail.com>
2020-06-07 00:26:38 +0800

    Update 视频替换音频.bat

--- bat/视频替换音频.bat
+++ bat/视频替换音频.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /D "%~dp0"
 
-set prefix=".\vendor\ffmpeg" -i "%~1" -i "%~2" 
 if /I "%~x1"==".mp4" (
     set middle=-map 0:v -map 1:a 
     set video=%~nx1
@@ -9,12 +8,9 @@ if /I "%~x1"==".mp4" (
     set middle=-map 0:a -map 1:v 
     set video=%~nx2
 )
-set suffix=-c copy %video%
 
-set cmd=%prefix%%middle%%suffix%
-echo on
+set cmd=".\vendor\ffmpeg" -i "%~1" -i "%~2" %middle% -c copy %video%
 %cmd%
-@echo off
 echo %cmd% >>"cmd.log"
 
 pause
