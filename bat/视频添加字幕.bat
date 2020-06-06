20fdcf5da980af76965c9f83ad9bed5278e41933
H4M5TER <zhuxx2003@gmail.com>
2020-06-07 00:20:53 +0800

    Update 视频添加字幕.bat等
    将字幕拷贝到工作目录下并重命名，允许中文字幕文件名。

--- bat/视频添加字幕.bat
+++ bat/视频添加字幕.bat
@@ -3,26 +3,26 @@ cd /D "%~dp0"
 
 if /I "%~x1"==".mp4" (
     set video=%~1
-    set subtitle=%~nx2
-    copy /Y "%~2" %subtitle%
+    set video_name=%~n1
+    copy /Y "%~2" subtitle.ass
 ) else (
     set video=%~2
-    set subtitle=%~nx1
-    copy /Y "%~1" %subtitle%
+    set video_name=%~n2
+    copy /Y "%~1" subtitle.ass
 )
 
-set prefix=".\vendor\ffmpeg" -i "%video%" -vf "ass=%subtitle%" -c:v h264 -profile:v main -level 4.2 
+set prefix=".\vendor\ffmpeg" -i "%video%" -vf "ass=subtitle.ass" -c:v h264 -profile:v main -level 4.2 
 set suffix1=-pass 1 -b:v 6000K -bufsize 6000K -maxrate 24000K -an -f mp4 -y NUL
-set suffix2=-pass 2 -b:v 6000K -bufsize 6000K -maxrate 24000K -c:a copy "%~n1_subtitled.mp4"
+set suffix2=-pass 2 -b:v 6000K -bufsize 6000K -maxrate 24000K -c:a copy "%video_name%_subtitled.mp4"
 
-echo on
 %prefix%%suffix1%
 %prefix%%suffix2%
-@echo off
+
 echo %prefix%%suffix1% >>"cmd.log"
 echo %prefix%%suffix2% >>"cmd.log"
+
 del ffmpeg2pass-0.log
 del ffmpeg2pass-0.log.mbtree
-del %subtitle%
+del subtitle.ass
 
 pause
--- bat/视频添加字幕水印.bat
+++ bat/视频添加字幕水印.bat
@@ -3,28 +3,29 @@ cd /D "%~dp0"
 
 if /I "%~x1"==".mp4" (
     set video=%~1
-    set subtitle=%~nx2
-    copy /Y "%~2" %subtitle%
+    set video_name=%~n1
+    copy /Y "%~2" subtitle.ass
 ) else (
     set video=%~2
-    set subtitle=%~nx1
-    copy /Y "%~1" %subtitle%
+    set video_name=%~n2
+    copy /Y "%~1" subtitle.ass
 )
 
 rem W-w equals to main_w - overlay_w
 rem 16*2=32 9*2=18
-set prefix=".\vendor\ffmpeg" -i "%video%" -i logo.png -filter_complex "[0:v]ass=%subtitle%[subtitled];[subtitled][1:v]overlay=W-w-32:18" -c:v h264 -profile:v main -level 4.2 
+rem 视logo.png分辨率为480*135
+set prefix=".\vendor\ffmpeg" -i "%video%" -i logo.png -filter_complex "[0:v]ass=subtitle.ass[subtitled];[subtitled][1:v]overlay=W-w-32:18" -c:v h264 -profile:v main -level 4.2 
 set suffix1=-pass 1 -b:v 6000K -bufsize 6000K -maxrate 24000K -an -f mp4 -y NUL
-set suffix2=-pass 2 -b:v 6000K -bufsize 6000K -maxrate 24000K -map 0:a -c:a copy "%~n1_encoded.mp4"
+set suffix2=-pass 2 -b:v 6000K -bufsize 6000K -maxrate 24000K -map 0:a -c:a copy "%video_name%_encoded.mp4"
 
-echo on
 %prefix%%suffix1%
 %prefix%%suffix2%
-@echo off
+
 echo %prefix%%suffix1% >>"cmd.log"
 echo %prefix%%suffix2% >>"cmd.log"
+
 del ffmpeg2pass-0.log
 del ffmpeg2pass-0.log.mbtree
-del %subtitle%
+del subtitle.ass
 
 pause
